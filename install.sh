304091b2cc08e61595916700aac9c77fc1fdf147
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-04-16 17:55:24 -0300

    improved xprofile creation
    the way it was, it was overriding the file. If the user already had anything in there, it would be replaced.
    
    changed to append touchegg to it instead.

--- install.sh
+++ install.sh
@@ -12,7 +12,5 @@ apt-get autoremove
 #copy touchegg configuration to default location
 cp ./touchegg.conf ~/.config/touchegg/
 
-#creates .xprofile on ~/ to start touchegg on session startup
-cat <<EOF > ~/.xprofile
-touchegg &
-EOF
+# updates ~/.xprofile to start touchegg on session startup
+echo 'touchegg &' >> ~/.xprofile
